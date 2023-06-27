b3929b22315ccc1278d7b56e1aceb349c47926eb
fas-fernando <benesinho14@gmail.com>
2023-06-27 11:45:41 -0300

    Incluido um recurso de paginação na timeline

--- App/Models/Tweet.php
+++ App/Models/Tweet.php
@@ -33,27 +33,47 @@ class Tweet extends Model {
 	//recuperar
 	public function getAll() {
 
-		$query = "
-			select 
-				t.id, 
-				t.id_usuario, 
-				u.nome, 
-				t.tweet, 
-				DATE_FORMAT(t.data, '%d/%m/%Y %H:%i') as data
-			from 
-				tweets as t
-				left join usuarios as u on (t.id_usuario = u.id)
-			where 
-				t.id_usuario = :id_usuario
-				or t.id_usuario in (select id_usuario_seguindo from usuarios_seguidores where id_usuario = :id_usuario)
-			order by
-				t.data desc
-		";
+		$query = "SELECT t.id, t.id_usuario, u.nome, t.tweet, DATE_FORMAT(t.data, '%d/%m/%Y %H:%i') as data
+			FROM tweets as t
+			left join usuarios as u on (t.id_usuario = u.id)
+			where t.id_usuario = :id_usuario or t.id_usuario in (select id_usuario_seguindo from usuarios_seguidores where id_usuario = :id_usuario)
+			order by t.data desc";
 
 		$stmt = $this->db->prepare($query);
 		$stmt->bindValue(':id_usuario', $this->__get('id_usuario'));
 		$stmt->execute();
 
 		return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+	}
+
+	public function getPorPagina($limit, $offset)
+	{
+		$query = "SELECT t.id, t.id_usuario, u.nome, t.tweet, DATE_FORMAT(t.data, '%d/%m/%Y %H:%i') as data
+		FROM tweets as t
+		left join usuarios as u on (t.id_usuario = u.id)
+		where t.id_usuario = :id_usuario or t.id_usuario in (select id_usuario_seguindo from usuarios_seguidores where id_usuario = :id_usuario)
+		order by t.data desc
+		limit $limit
+		offset $offset";
+
+		$stmt = $this->db->prepare($query);
+		$stmt->bindValue(':id_usuario', $this->__get('id_usuario'));
+		$stmt->execute();
+
+		return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+	}
+
+	public function getTotalRegistros()
+	{
+		$query = "SELECT count(*) AS total
+		FROM tweets as t
+		left join usuarios as u on (t.id_usuario = u.id)
+		where t.id_usuario = :id_usuario or t.id_usuario in (select id_usuario_seguindo from usuarios_seguidores where id_usuario = :id_usuario)";
+
+		$stmt = $this->db->prepare($query);
+		$stmt->bindValue(':id_usuario', $this->__get('id_usuario'));
+		$stmt->execute();
+
+		return $stmt->fetch(\PDO::FETCH_ASSOC);
 	}
 }
